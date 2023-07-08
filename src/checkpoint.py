bf54a48fe7ff4704a0d15f71881f0785a08991b5
Matt Markwald <mmarkwald01@gmail.com>
2023-07-08 05:02:50 -0400

    creates directory data if it does not exist

--- src/checkpoint.py
+++ src/checkpoint.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import os
+
 checkpoint_file = 'data/checkpoint.txt'
 
 # Save the checkpoint ID to a file
 def save_checkpoint(checkpoint_id):
+    create_directory_if_not_exists()
     with open(checkpoint_file, 'w') as file:
         file.write(checkpoint_id)
 
@@ -15,3 +18,9 @@ def load_checkpoint():
         return checkpoint_id
     except FileNotFoundError:
         return None
+
+# Create the data directory if it doesn't exist
+def create_directory_if_not_exists():
+    directory = os.path.dirname(checkpoint_file)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
